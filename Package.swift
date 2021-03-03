412fc1ce09dafe8bfc3d539d148a5afff1a86250
Moi Gutierrez <moyoteg@gmail.com>
2021-03-02 18:38:40 -0600

    updated Utilities

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "Utilities", url: "https://git.nikolamotor.com/core/mobile/ios/swift/utilities", .branch("master")),
+        .package(name: "Utilities", url: "https://github.com/moyoteg/SwiftUtilities", .branch("master")),
         .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.2")),
     ],
     targets: [
