b24a86141528f3c70be3de874fb22c25e2a2c05c
Moi Gutierrez <moyoteg@gmail.com>
2021-06-27 17:53:32 -0500

    added log types

--- Sources/CloudyLogs/Logger.swift
+++ Sources/CloudyLogs/Logger.swift
@@ -13,24 +13,38 @@ import LocalConsole
 /// responsible for handling all log creation.
 @objc public class Logger: NSObject {
 
+    public enum LogType: String {
+        case error      = "🛑"
+        case warning    = "🟠"
+        case info       = "🟢"
+        
+        public var string: String { return rawValue }
+    }
+    
+    public static var shared = Logger()
+    
+    public var logToLocalConsole = false {
+        didSet {
+            Logger.localConsoleManager.isVisible = logToLocalConsole
+        }
+    }
+    
     public static let localConsoleManager = LCManager.shared
 
     private static let logQueue = DispatchQueue.init(label: "logQueue")
     
+    public static var localConsoleLogOperationQueue = OperationQueue()
+
     /// logs message to log.
     ///
     /// - Parameters:
     ///   - message: string representation of log.
     ///   - type: the type that is sending the log creation.
-    @objc static public func log(_ message: String, type: Any? = nil) {
+    static public func log(_ message: String,
+                                 type: Any? = nil,
+                                 logType: LogType = .info) {
         logQueue.async {
-            Logger.attemptToLog(message, type: type)
-        }
-
-        DispatchQueue.main.async {
-            // Print items to the console view.
-            // TODO: fix performance issues
-            localConsoleManager.print(message)
+            Logger.attemptToLog(message, type: type, logType: logType)
         }
     }
 
@@ -39,9 +53,12 @@ import LocalConsole
     /// - Parameters:
     ///   - message: intended message for the log.
     ///   - typeName: name of the type creating the log.
-    fileprivate static func attemptToLog(_ message: String, type: Any?) {
+    fileprivate static func attemptToLog(_ message: String,
+                                         type: Any?,
+                                         logType: LogType) {
 
         let sanitizedMessage = sanitize(message)
+        let log = "\(Date()) \(logType.rawValue): \(sanitizedMessage)"
 
         switch EnvironmentVariables.VerboseLevel.value {
         case .silent:
@@ -49,24 +66,36 @@ import LocalConsole
             // logging
             break
         case .verbose:
-            guard let type = type else {
-                // otherwise only use the message.
-                let log = "\(Date()): \(sanitizedMessage)"
-                print(log, to: &TextFileLogger.logger)
-                print(log)
+            if let type = type {
+                // if get type name has a valid string use it for the name.
+                let logWWithType = "\(String(describing: type)): \(log)"
+                print(logWWithType, to: &TextFileLogger.logger)
+                print(logWWithType)
                 return
             }
-            // sanitation -> we need to add sanitation logic here.
-            // if get type name has a valid string use it for the name.
-            let log = "\(String(describing: type)): \(sanitizedMessage)"
+            
+            // otherwise only use the message.
             print(log, to: &TextFileLogger.logger)
             print(log)
+            
         case .none:
             // must use "print" since we don't want a recurseive call
             if !printedNoVerboseLevelFound {
                 print("Logger: No Environment Variable set for Logging.")
             }
         }
+        
+        let logOperation = BlockOperation {
+            DispatchQueue.main.async {
+                // Print items to the console view.
+                // TODO: fix performance issues
+                if Logger.shared.logToLocalConsole {
+                    localConsoleManager.print(log)
+                }
+            }
+        }
+        
+        Logger.localConsoleLogOperationQueue.addOperation(logOperation)
     }
 
     /// removes any sensitive information from the message.
@@ -100,7 +129,7 @@ import LocalConsole
                     succes()
                 }
             case .failure(let error):
-                Logger.log("Logger: multipartFormData failed: \(error)")
+                Logger.log("Logger: multipartFormData failed: \(error)", logType: .error)
             }
         }.uploadProgress { (progress) in
             //Print progress
